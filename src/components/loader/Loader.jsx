17e87efada267d3c0492e46805cb68afb8ae99fe
Justin Hadinata <justinhadinata.23@gmail.com>
2025-06-03 01:34:53 +0700

    Change the color of the loader

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -3,7 +3,7 @@ import { BounceLoader } from "react-spinners";
 function Loader() {
   return (
     <div className="fixed  inset-0 z-50 flex items-center justify-center backdrop-blur-sm ">
-      <BounceLoader size={80} color="##1D3B5C" />
+      <BounceLoader size={80} color="#1D3B5C" />
     </div>
   );
 }
