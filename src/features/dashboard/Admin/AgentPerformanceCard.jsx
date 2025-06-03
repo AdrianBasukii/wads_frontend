fc4e6e442385da8c68855241297ab6ca31700b66
Justin Hadinata <justinhadinata.23@gmail.com>
2025-06-03 10:44:28 +0700

    Fix r undefined error

--- src/features/dashboard/Admin/AgentPerformanceCard.jsx
+++ src/features/dashboard/Admin/AgentPerformanceCard.jsx
@@ -1,37 +1,42 @@
-import CardTitle from "../CardTitle"
-import AgentPerformanceContent from "./AgentPerformanceContent"
-import AdminDropdown from "./AdminDropdown"
+import CardTitle from "../CardTitle";
+import AgentPerformanceContent from "./AgentPerformanceContent";
+import AdminDropdown from "./AdminDropdown";
 import Loader from "../../../components/loader/Loader";
-import { useAdminDashboardContext } from "../../../contexts/AdminDashboardContext"
-import { useState } from "react"
+import { useAdminDashboardContext } from "../../../contexts/AdminDashboardContext";
+import { useState } from "react";
 
-export default function AgentPerformanceCard(){
-
-    const { agentPerformance, agentPerformanceLoading } = useAdminDashboardContext();
-    let options
-    if(!agentPerformanceLoading) options = agentPerformance.performance.map((agent, index) => {
-        const { firstName, lastName, userId } = agent.assignedTo;
-        const shortId = userId.slice(-4);
-        return {
-            label: `${firstName} ${lastName} (#${shortId})`,
-            value: index
-        };
+export default function AgentPerformanceCard() {
+  const { agentPerformance, agentPerformanceLoading } =
+    useAdminDashboardContext();
+  let options;
+  if (!agentPerformanceLoading)
+    options = agentPerformance.performance.map((agent, index) => {
+      const { firstName, lastName, userId } = agent.assignedTo;
+      const shortId = userId.slice(-4);
+      return {
+        label: `${firstName} ${lastName} (#${shortId})`,
+        value: index,
+      };
     });
 
-    const [selectedOption, setSelectedOption] = useState(0)
+  const [selectedOption, setSelectedOption] = useState(0);
 
-    const handleOptionChange = (newOption) => {
-        setSelectedOption(newOption);
-    }
+  const handleOptionChange = (newOption) => {
+    setSelectedOption(newOption);
+  };
 
-    if(agentPerformanceLoading) return <Loader />
+  if (agentPerformanceLoading) return <Loader />;
 
-    return(
-        <div className="bg-white rounded-sm shadow-md border border-neutral-200">
-            <CardTitle title="Agent Performance">
-                <AdminDropdown options={options} onChange={handleOptionChange}/>
-            </CardTitle>
-            <AgentPerformanceContent data={agentPerformance.performance[selectedOption]}/>
+  return (
+    <div className="bg-white rounded-sm shadow-md border border-neutral-200">
+      <CardTitle title="Agent Performance">
+        <div className="max-w-[250px] w-full">
+          <AdminDropdown options={options} onChange={handleOptionChange} />
         </div>
-    )
+      </CardTitle>
+      <AgentPerformanceContent
+        data={agentPerformance.performance[selectedOption]}
+      />
+    </div>
+  );
 }
