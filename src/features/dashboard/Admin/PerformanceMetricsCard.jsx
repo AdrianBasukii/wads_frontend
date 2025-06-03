b6e06e3204edf216b530f24f25981f67c302993e
Justin Hadinata <justinhadinata.23@gmail.com>
2025-06-03 21:53:13 +0700

    Remove the debug button

--- src/features/dashboard/Admin/PerformanceMetricsCard.jsx
+++ src/features/dashboard/Admin/PerformanceMetricsCard.jsx
@@ -42,7 +42,6 @@ export default function PerformanceMetricsCard() {
           />
         </div>
       </CardTitle>
-      <button onClick={() => console.log(serverUptime)}>Test</button>
 
       {selectedOption === "Response Time" && (
         <>
